76d1f73740b69e9eb13adf75482f01bad9d10e8c
ashikurriyal <riyal.ashikur@gmail.com>
2025-02-19 22:17:34 +0600

    updated the text

--- src/app/about/history/vision/page.jsx
+++ src/app/about/history/vision/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl text-blue-700 font-medium">Vission Page</h1>
+      <h1 className="text-center text-4xl text-blue-700 font-medium">Vision Page</h1>
     </div>
   );
 };
