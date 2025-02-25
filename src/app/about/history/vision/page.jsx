2163ab0a52d500d1c405398e80d3b99d8e624158
ashikurriyal <riyal.ashikur@gmail.com>
2025-02-25 18:48:45 +0600

    updated

--- src/app/about/history/vision/page.jsx
+++ src/app/about/history/vision/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl text-blue-700 font-medium">Vision Page</h1>
+      <h1 className="text-center text-4xl text-blue-700 font-medium">Vision Details Page</h1>
     </div>
   );
 };
