b69677c9171094df4f8cbc18e54eacd38d476bd6
ashikurriyal <riyal.ashikur@gmail.com>
2025-02-28 22:58:50 +0600

    updated the error page

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -12,7 +12,7 @@ const error = () => {
               404
             </h1>
             <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-              Something's missing.
+              Something's missing. Updating
             </p>
             <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
               Sorry, we can't find that page. You'll find lots to explore on the
